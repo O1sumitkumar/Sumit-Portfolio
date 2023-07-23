4cf9659b24c2751b97176cfb7b2725ccf0d46c13
O1sumitkumar <o1sumeetkumar@gmail.com>
2023-07-23 22:55:38 +0530

    gg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,14 +16,23 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a
+                href="https://www.linkedin.com/in/01sumitkumar"
+                target="_blank"
+              >
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              <a href="https://www.instagram.com/o1sumitkumar" target="_blank">
+                <img src={navIcon3} alt="Icon" />
+              </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
